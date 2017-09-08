931f239f6d83cdea625f764b551e59e2d54c7c34
hym <1349456160@qq.com>
2017-09-08 16:33:29 +0800

    界面设计完成，线程部分缺少处理

--- app/src/main/java/com/example/lenovo/handler/MainActivity.java
+++ app/src/main/java/com/example/lenovo/handler/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.lenovo.handler;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int UPADTE_TEXT=1;
+    private TextView text;
+    private EditText editText;
+    private String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainlayout);
+        text=(TextView) findViewById(R.id.output);
+        editText=(EditText) findViewById(R.id.input);
+        s=editText.getText().toString();
+        Button changetext=(Button) findViewById(R.id.changetext);
+        changetext.setOnClickListener(this);
+    }
+    private Handler handler=new Handler(){
+      public void handleMessage(Message msg){
+          switch (msg.what) {
+              case UPADTE_TEXT:
+                  text.setText(s);
+                  break;
+              default:
+                  break;
+          }
+      }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.changetext:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
